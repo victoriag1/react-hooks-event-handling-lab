73639fe1899f0d9bb9bbccd6dcd937b92d50fede
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-04-25 21:20:50 -0700

    commit tree fe7ea853f4f9d5345a05476b61e3c533aae0f0c3

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focusEvent(){
+        console.log('Good!')
+    }
+function blurEvent(){
+    console.log('Hey! Eyes on me!')
+}
+    return (
+
+     <button onFocus={focusEvent} onBlur={blurEvent}>
+         Eyes on me </button>
+
+    )
+       
+    
+}
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    function HandleChange(event){
+        console.log("Entering password...")
+
+    }
+    return (
+        <input type="password"
+        onChange={HandleChange}
+        
+        />
+    )
+}
+
+export default Keypad
